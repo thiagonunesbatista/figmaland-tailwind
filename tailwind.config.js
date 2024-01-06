7966c28c940ef35e469f6bab1da977a7761734b0
Thiago Nunes Batista <67487679+thiagonunesbatista@users.noreply.github.com>
2024-01-06 11:58:22 -0300

    feat: add text font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        graphik: ['Graphik', 'sans-serif'],
+        'graphik-bold': ['Graphik-bold', 'sans-serif']
+      }
+    }
   },
   plugins: []
 }
